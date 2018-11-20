12b528af0f074021741370ce66153b1451faa670
winterhart <m.user.work@gmail.com>
2018-11-20 11:05:30 -0500

    Challenges POJOs final changes

--- poke/src/org/soen387/app/Domain/POJO/challenge/IChallenge.java
+++ poke/src/org/soen387/app/Domain/POJO/challenge/IChallenge.java
@@ -3,5 +3,25 @@ package org.soen387.app.Domain.POJO.challenge;
 import org.dsrg.soenea.domain.interf.IDomainObject;
 
 public interface IChallenge extends IDomainObject<Long> {
+	
+	public Long getChallengerId();
+
+	public void setChallengerId(Long challengerId);
+
+	public Long getChallengeeId();
+
+	public void setChallengeeId(Long challengeeId);
+
+	public ChallengeStatus getStatus();
+
+	public void setStatus(ChallengeStatus status);
+
+	public Long getDeckInitializer();
+	
+	public int getChallengeStatus() 
+			throws InvalidChallengeStatusException;
+	
+	public void setChallengeStatus(int status) 
+			throws InvalidChallengeStatusException;
 
 }
