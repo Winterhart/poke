1551272f0b62851c6c3ac9e6bedb754c5856306e
winterhart <m.user.work@gmail.com>
2018-11-22 13:08:29 -0500

    #1 Addition of basic connection setting for database

--- poke/src/org/soen387/app/Util/DbSetup.java
+++ poke/src/org/soen387/app/Util/DbSetup.java
@@ -1,5 +1,7 @@
 package org.soen387.app.Util;
 
+import org.dsrg.soenea.service.tdg.UserTDG;
+import org.dsrg.soenea.service.threadLocal.DbRegistry;
 import org.soen387.app.Presentation.FrontController;
 /**
  * Inspired by Stuart Thiel's Thesis : 
@@ -17,15 +19,63 @@ public abstract class DbSetup {
 	}
 	protected static void createDatabase() {
 		
+		try {
+			
+			UserTDG.createTable();
+			//TODO: Add other table
+			
+		}catch(Exception ee) {
+			System.out.println(" ERROR WHILE CREATING DATABASE ");
+			ee.printStackTrace();
+		}
+		
 	}
 	protected static void removeTables() {
-		
+		try {
+			UserTDG.dropTable();
+			//TODO: add Drop to other table
+		}catch(Exception ee) {
+			System.out.println(" ERROR WHILE DELETING DATABASE");
+		}
 	}
 	protected static void startLogging() {
+		//TODO: Add logging implementation
+	}
+	protected static void startTransaction() {
 		
 	}
+	
 	public static void setup() {
 		startLogging();
+		FrontController.prepareDBRegsitry("");
+		FrontController.setupUoW();
+		try {
+			
+			startTransaction();
+			createDatabase();
+			
+		}catch(Exception ee) {
+			ee.printStackTrace();
+			System.exit(1);
+		}
 	}
+	
+	public static void tearDown() {
+		try {
+			startTransaction();
+			removeTables();
+			finishTransaction();
+			DbRegistry.closeDbConnectionIfNeeded();
+		}catch(Exception ee) {
+			ee.printStackTrace();
+			
+		}
+	}
+	
+	public static void finishTransaction() {
+		
+	}
+	
+
 
 }
