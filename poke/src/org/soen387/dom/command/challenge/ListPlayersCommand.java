e143d09b5372c8b5aa08c7df40129a0810216e20
winterhart <m.user.work@gmail.com>
2018-11-29 18:41:22 -0500

    #10 addition of JSON parser lib (gson), #7 completed List Players

--- poke/src/org/soen387/dom/command/challenge/ListPlayersCommand.java
+++ poke/src/org/soen387/dom/command/challenge/ListPlayersCommand.java
@@ -1,10 +1,18 @@
 package org.soen387.dom.command.challenge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.dsrg.soenea.domain.command.CommandException;
 import org.dsrg.soenea.domain.command.impl.ValidatorCommand;
 import org.dsrg.soenea.domain.helper.Helper;
+import org.dsrg.soenea.domain.user.IUser;
+import org.dsrg.soenea.domain.user.UserProxy;
+import org.dsrg.soenea.domain.user.mapper.UserInputMapper;
 
-import net.minidev.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 
 public class ListPlayersCommand extends ValidatorCommand  {
 
@@ -16,15 +24,9 @@ public class ListPlayersCommand extends ValidatorCommand  {
 	@Override
 	public void process() throws CommandException {
 		try {
-			JSONObject playersJSON = new JSONObject();
-			playersJSON.put("id", 1);
-			playersJSON.put("user", "bobo");
-//			playersJSON.put("id", 2);
-//			playersJSON.put("user", "coucou");
-//			playersJSON.put("id", 3);
-//			playersJSON.put("user", "test");
-			
-			String players = "Hello";
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			List<UserJsonFormat> userJson = convertToFormat( UserInputMapper.findAll());
+			String players = gson.toJson(userJson);
 			helper.setRequestAttribute("players", players);
 		}catch(Exception ee) {
 			ee.printStackTrace();
@@ -33,6 +35,26 @@ public class ListPlayersCommand extends ValidatorCommand  {
 			throw new CommandException(ee);
 		}
 		
+	}
+	
+	private List<UserJsonFormat> convertToFormat(List<IUser> users){
+		List<UserJsonFormat> usersJ = new ArrayList<UserJsonFormat>();
+		for(IUser user : users) {
+			UserJsonFormat u = new UserJsonFormat(user.getUsername(), user.getId());
+			usersJ.add(u);
+		}
+		
+		return usersJ;
+
+	}
+	
+	protected class UserJsonFormat{
+		private String user;
+		private Long id;
+		UserJsonFormat(String user, Long id){
+			this.user = user;
+			this.id = id;
+		}
 	}
 
 }
